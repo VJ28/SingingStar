fdc71c7548faa21e88732b21745fe20fb45cee4e
vijaymourya28 <vijay.mourya@carwale.com>
2020-06-06 16:48:28 +0530

    validations added

--- src/server.js
+++ src/server.js
@@ -1,10 +1,7 @@
 import express from "express";
 import compression from "compression";
 import ssr from "./backend/routes/ssr";
-import http2 from "../node_modules/http2";
 import bodyParser from "body-parser";
-import http from "http";
-import fs from "fs";
 import fileUpload from "express-fileupload";
 import path from "path";
 import AWS from "aws-sdk";
@@ -12,11 +9,6 @@ if (process.env.NODE_ENV !== "production") {
   require("dotenv").config({ path: "E:/SingingStar/src/.env" });
 }
 const app = express();
-// require("express-http2-workaround")({
-//   express: express,
-//   http2: http2,
-//   app: app,
-// });
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 app.use(fileUpload());
@@ -29,24 +21,6 @@ let port = process.env.PORT || 3000;
 app.listen(port, "0.0.0.0", 511, function () {
   console.log("Express HTTP/1 server started on port: " + port);
 });
-
-// const options = {
-//   key: fs.readFileSync(
-//     path.join(__dirname, "certificates", "localhost-privkey.pem")
-//   ),
-//   cert: fs.readFileSync(
-//     path.join(__dirname, "certificates", "localhost-cert.pem")
-//   ),
-//   ca: fs.readFileSync(
-//     path.join(__dirname, "certificates", "localhost-cert.pem")
-//   ),
-//   allowHTTP1: true,
-// };
-// let server = http2.createServer(options, app);
-
-// server.listen(3030, function listenHandler() {
-//   console.info(`Running on 3030...`);
-// });
 
 AWS.config.region = process.env.AWS_REGION; // Region
 AWS.config.credentials = new AWS.CognitoIdentityCredentials({
